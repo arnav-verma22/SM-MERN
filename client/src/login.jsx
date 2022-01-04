856794007bb562e5d44e1c842cfa4133753ed89b
arnav-verma22 <arn.verma22@gmail.com>
2022-01-05 00:52:34 +0530

    client side setup

---
+++ client/src/login.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+//import { Link } from "react-router-dom";
+
+import "./form-validation.css";
+
+class NavBar extends Component {
+  render() {
+    return (
+            <>
+                
+
+            </>
+        );
+    }
+}
+  
+  export default NavBar;
