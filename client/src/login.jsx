cc7810431132c0f2a8dc7c88d5aef4cd9c6cdb2a
arnav-verma22 <arn.verma22@gmail.com>
2022-01-05 15:05:20 +0530

    fixed bugs and added logo

--- client/src/login.jsx
+++ client/src/login.jsx
@@ -9,18 +9,18 @@ class Login extends Component {
             <React.Fragment>
                 <main className="form-signin">
                 <form>
-                    
+                    <img className="mb-4" src="logo4.png" alt="" width="72" height="72" align="center" />
                     <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
 
-                    <div >
-                    <label htmlFor="floatingInput">Email address</label>
+                    <div className="form-floating">
+                    
                     <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
-                    
+                    <label htmlFor="floatingInput">Email address</label>
                     </div>
-                    <div >
-                    <label htmlFor="floatingPassword">Password</label>
-                    <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+                    <div className="form-floating">
                     
+                    <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+                    <label htmlFor="floatingPassword">Password</label>
                     </div>
 
                     <div className="checkbox mb-3">
