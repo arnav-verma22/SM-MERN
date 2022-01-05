38a29e6dfaa61f20af7afadf906c0d4bfbd21d07
arnav-verma22 <arn.verma22@gmail.com>
2022-01-05 12:54:45 +0530

    client side setup and signin component

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Login from "./login";
+
+function App() {
+  return (
+    <Login />
+  );
+}
+
+export default App;
--- client/src/login.jsx
+++ client/src/login.jsx
@@ -1,17 +1,41 @@
 import React, { Component } from "react";
 //import { Link } from "react-router-dom";
 
-import "./form-validation.css";
+import "./signin.css";
 
-class NavBar extends Component {
+class Login extends Component {
   render() {
     return (
-            <>
-                
+            <React.Fragment>
+                <main className="form-signin">
+                <form>
+                    
+                    <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
 
-            </>
+                    <div >
+                    <label htmlFor="floatingInput">Email address</label>
+                    <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+                    
+                    </div>
+                    <div >
+                    <label htmlFor="floatingPassword">Password</label>
+                    <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+                    
+                    </div>
+
+                    <div className="checkbox mb-3">
+                    <label>
+                        <input type="checkbox" value="remember-me" /> Remember me
+                    </label>
+                    </div>
+                    <button className="btn btn-outline-success" type="submit">Sign in</button>
+                    <p className="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
+                </form>
+                </main>
+
+            </React.Fragment>
         );
     }
 }
   
-  export default NavBar;
+  export default Login;
